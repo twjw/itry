dc209a7986513f7c265163aafc0d2761092f6b26
twjw <aaaawjwa@gmail.com>
2024-08-09 23:47:27 +0800

    refactor: 引入 gin 並初始化

---
+++ go/contentsystem/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		fmt.Printf("r run error = %v", err)
+		return
+	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
